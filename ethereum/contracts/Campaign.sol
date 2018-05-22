17873a913068cfa6c89873578eca721e14b9251a
Abhishek <201651003@iiitvadodara.ac.in>
2018-05-22 14:32:11 +0530

    Added contribute and createRequest functions.

--- ethereum/contracts/Campaign.sol
+++ ethereum/contracts/Campaign.sol
@@ -41,5 +41,22 @@ contract Campaign{
         minimumContribution = minimum;
     }
     
-   
+     function contribute() public payable{
+        require(msg.value > minimumContribution);
+        approvers[msg.sender]= true;
+        approversCount++;
+    }
+    
+    function createRequest(string description, uint value, address recipient) public restricted {
+        
+        Request memory newRequest = Request({
+            description : description,
+            value: value,
+            recipient : recipient,
+            complete: false,
+            approvalCount: 0
+        });
+
+        requests.push(newRequest);
+    } 
 }
