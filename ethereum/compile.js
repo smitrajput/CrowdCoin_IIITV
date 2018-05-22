f8201e762f1f71f18609a1fe18f849d7ae5920f7
Smit Rajput <smitrajputtd@gmail.com>
2018-05-22 16:28:51 +0530

    Finish compile script

--- ethereum/compile.js
+++ ethereum/compile.js
@@ -10,3 +10,11 @@ const source = fs.readFileSync(campaignPath, 'utf8');
 const output = solc.compile(source, 1).contracts;
 
 fs.ensureDirSync(buildPath);
+
+console.log(output);
+for (let contract in output) {
+  fs.outputJsonSync(
+    path.resolve(buildPath, contract.replace(':','') + '.json'),
+    output[contract]
+  );
+}
