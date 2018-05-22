b0a94c1eaaa4fe9aa1b282f6bc38f7af2f531032
Smit Rajput <smitrajputtd@gmail.com>
2018-05-22 13:44:18 +0530

    Add CampaignFactory contract

--- ethereum/contracts/Campaign.sol
+++ ethereum/contracts/Campaign.sol
@@ -1,6 +1,19 @@
 pragma solidity ^0.4.17;
 
 
+contract CampaignFactory {
+    address[] public deployedCampaigns;
+
+    function createCampaign(uint minimum) public {
+        address newCampaign = new Campaign(minimum, msg.sender);
+        deployedCampaigns.push(newCampaign);
+    }
+
+    function getDeployedCampaigns() public view returns (address[]) {
+        return deployedCampaigns;
+    }
+}
+
 
 contract Campaign{
     struct Request{
